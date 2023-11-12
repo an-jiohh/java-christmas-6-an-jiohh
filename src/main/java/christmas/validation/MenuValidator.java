735e2e0b3e6a4ecbe3a93b25d51f129e5916181d
jiohh <dkswlgh99@naver.com>
2023-11-13 05:23:20 +0900

    feat(validation): 메뉴 검증 기능

---
+++ src/main/java/christmas/validation/MenuValidator.java
@@ -0,0 +1,45 @@
+package christmas.validation;
+
+import static christmas.constants.Constants.MAX_MENU_COUNT;
+
+import christmas.constants.MenuCategory;
+import christmas.constants.MenuItem;
+import christmas.exception.BeverageOnlyOrderException;
+import christmas.exception.MenuLimitExceededException;
+import christmas.exception.MenuNotFoundException;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class MenuValidator {
+    public static void validateMenu(Map<String, Integer> noneConstantsMenus) {
+        checkExistenceMenu(noneConstantsMenus);
+        checkCountOverflow(noneConstantsMenus);
+    }
+
+    public static void checkOrderOnlyBeverage(EnumMap<MenuItem, Integer> menus) {
+        if (isBeverageOnlyOrder(menus)) {
+            throw BeverageOnlyOrderException.exception;
+        }
+    }
+
+    private static boolean isBeverageOnlyOrder(EnumMap<MenuItem, Integer> menus) {
+        return menus.keySet().stream().allMatch(item -> item.getCategory() == MenuCategory.BEVERAGE);
+    }
+
+    private static void checkExistenceMenu(Map<String, Integer> noneConstantsMenus) {
+        List<String> menuNames = MenuItem.getNames();
+        boolean isNoneExistence = noneConstantsMenus.keySet().stream()
+                .anyMatch(menuName -> !menuNames.contains(menuName));
+        if (isNoneExistence) {
+            throw MenuNotFoundException.exception;
+        }
+    }
+
+    private static void checkCountOverflow(Map<String, Integer> noneConstantsMenus) {
+        int CountSum = noneConstantsMenus.values().stream().mapToInt(Integer::intValue).sum();
+        if (MAX_MENU_COUNT < CountSum) {
+            throw MenuLimitExceededException.exception;
+        }
+    }
+}
