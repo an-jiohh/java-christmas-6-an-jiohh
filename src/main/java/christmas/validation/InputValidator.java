45e852a6af4790947fd9775c96bb2cc9446146d1
jiohh <dkswlgh99@naver.com>
2023-11-13 03:26:06 +0900

    feat(test): 메뉴 입력 기능 테스트

--- src/main/java/christmas/validation/InputValidator.java
+++ src/main/java/christmas/validation/InputValidator.java
@@ -1,13 +1,49 @@
 package christmas.validation;
 
+import static christmas.constants.Constants.COUNT_POSITION;
+import static christmas.constants.Constants.INITIAL_NUM;
+import static christmas.constants.Constants.MENUS_SEPARATOR;
+import static christmas.constants.Constants.MENUS_SEPARATOR_DUPLICATE;
+import static christmas.constants.Constants.MENU_COUNT_SEPARATOR;
+import static christmas.constants.Constants.MENU_COUNT_SEPARATOR_DUPLICATE;
+import static christmas.constants.Constants.MENU_COUNT_SPLIT_LENGTH;
+import static christmas.constants.Constants.MENU_POSITION;
+
 import christmas.exception.IncludeCharException;
 import christmas.exception.InputBlankException;
+import christmas.exception.InvalidMenuCountSeparatorException;
+import christmas.exception.InvalidMenusSeparatorException;
+import java.util.List;
 
 public class InputValidator {
 
     public void validateNumber(String readLine) {
         checkBlack(readLine);
         checkDigit(readLine);
+    }
+
+    public void validateMenuLine(String readLine) {
+        checkBlack(readLine);
+        checkMenusSeparator(readLine);
+    }
+
+    public void validateMenuCountSplit(List<String> menus) {
+        menus.forEach(menuAndCount -> {
+            checkMenuCountContainSeparator(menuAndCount);
+            checkMenuCountSeparatorPosition(menuAndCount);
+            checkMenuCountSplitPosition(menuAndCount);
+        });
+    }
+
+    public void validateMenu(List<String[]> menus) {
+        menus.forEach(menu -> checkBlack(menu[MENU_POSITION]));
+    }
+
+    public void validateCount(List<String[]> menus) {
+        menus.forEach(menu -> {
+            checkBlack(menu[COUNT_POSITION]);
+            checkDigit(menu[COUNT_POSITION]);
+        });
     }
 
     private void checkDigit(String readLine) {
@@ -21,6 +57,43 @@ public class InputValidator {
     private void checkBlack(String readLine) {
         if (readLine == null || readLine.isBlank()) {
             throw InputBlankException.exception;
+        }
+    }
+
+    private void checkMenusSeparator(String readLine) {
+        if (MENUS_SEPARATOR.charAt(INITIAL_NUM) == readLine.charAt(INITIAL_NUM)) {
+            throw InvalidMenusSeparatorException.exception;
+        }
+        if (MENUS_SEPARATOR.charAt(INITIAL_NUM) == readLine.charAt(readLine.length() - 1)) {
+            throw InvalidMenusSeparatorException.exception;
+        }
+        if (readLine.contains(MENUS_SEPARATOR_DUPLICATE)) {
+            throw InvalidMenusSeparatorException.exception;
+        }
+    }
+
+    private void checkMenuCountContainSeparator(String menuAndCount) {
+        if (!menuAndCount.contains(MENU_COUNT_SEPARATOR)) {
+            throw InvalidMenuCountSeparatorException.exception;
+        }
+    }
+
+    private void checkMenuCountSeparatorPosition(String menuAndCount) {
+        if (MENU_COUNT_SEPARATOR.charAt(INITIAL_NUM) == menuAndCount.charAt(INITIAL_NUM)) {
+            throw InvalidMenuCountSeparatorException.exception;
+        }
+        if (MENU_COUNT_SEPARATOR.charAt(INITIAL_NUM) == menuAndCount.charAt(menuAndCount.length() - 1)) {
+            throw InvalidMenuCountSeparatorException.exception;
+        }
+        if (menuAndCount.contains(MENU_COUNT_SEPARATOR_DUPLICATE)) {
+            throw InvalidMenuCountSeparatorException.exception;
+        }
+    }
+
+    private void checkMenuCountSplitPosition(String menuAndCount) {
+        String[] split = menuAndCount.split(MENU_COUNT_SEPARATOR);
+        if (split.length != MENU_COUNT_SPLIT_LENGTH) {
+            throw InvalidMenuCountSeparatorException.exception;
         }
     }
 }
