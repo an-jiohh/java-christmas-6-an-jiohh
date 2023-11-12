cc94dadd064c2fde505a18121820f3391089c92f
jiohh <dkswlgh99@naver.com>
2023-11-13 00:52:38 +0900

    feat(service): 날짜 가져오기 기능 통합

---
+++ src/main/java/christmas/service/DateService.java
@@ -0,0 +1,26 @@
+package christmas.service;
+
+import christmas.domain.Date;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class DateService {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public DateService() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public Date fetchValidatedDate() {
+        while (true) {
+            try {
+                int number = inputView.readNumberData();
+                return new Date(number);
+            } catch (IllegalArgumentException exception) {
+                outputView.printErrorMessage(exception);
+            }
+        }
+    }
+}
