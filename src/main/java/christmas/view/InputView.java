8696e6c9fa5993a68ae3c80f77b0e8c9ce0e3865
jiohh <dkswlgh99@naver.com>
2023-11-13 03:25:39 +0900

    feat(view): 메뉴 입력 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,8 +1,16 @@
 package christmas.view;
 
+import static christmas.constants.Constants.COUNT_POSITION;
+import static christmas.constants.Constants.MENUS_SEPARATOR;
+import static christmas.constants.Constants.MENU_COUNT_SEPARATOR;
+import static christmas.constants.Constants.MENU_POSITION;
+
 import camp.nextstep.edu.missionutils.Console;
 import christmas.exception.DataTypeRangeException;
+import christmas.exception.DuplicateMenuException;
 import christmas.validation.InputValidator;
+import java.util.HashMap;
+import java.util.List;
 
 public class InputView {
 
@@ -18,7 +26,38 @@ public class InputView {
         inputValidator.validateNumber(readLine);
         return validatedNumberParser(readLine);
     }
-    
+
+    public HashMap<String, Integer> readMenuData() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String readLine = Console.readLine();
+        List<String> menuAndCounts = splitMenuReadLine(readLine);
+        List<String[]> separatedMenus = splitMenuAndCount(menuAndCounts);
+        return menuAndCountConverter(separatedMenus);
+    }
+
+    private List<String> splitMenuReadLine(String readLine) {
+        inputValidator.validateMenuLine(readLine);
+        return List.of(readLine.split(MENUS_SEPARATOR));
+    }
+
+    private List<String[]> splitMenuAndCount(List<String> menuAndCounts) {
+        inputValidator.validateMenuCountSplit(menuAndCounts);
+        return menuAndCounts.stream().map(menuAndCount -> menuAndCount.split(MENU_COUNT_SEPARATOR)).toList();
+    }
+
+    private HashMap<String, Integer> menuAndCountConverter(List<String[]> separatedMenus) {
+        inputValidator.validateMenu(separatedMenus);
+        inputValidator.validateCount(separatedMenus);
+        HashMap<String, Integer> menus = new HashMap<>();
+        separatedMenus.forEach(separatedMenu -> {
+            if (menus.containsKey(separatedMenu[MENU_POSITION])) {
+                throw DuplicateMenuException.exception;
+            }
+            menus.put(separatedMenu[MENU_POSITION], validatedNumberParser(separatedMenu[COUNT_POSITION]));
+        });
+        return menus;
+    }
+
     private int validatedNumberParser(String readLine) {
         try {
             return Integer.parseInt(readLine);
--- src/test/java/christmas/validation/InputValidatorTest.java
+++ src/test/java/christmas/validation/InputValidatorTest.java
@@ -2,11 +2,17 @@ package christmas.validation;
 
 import christmas.exception.IncludeCharException;
 import christmas.exception.InputBlankException;
+import christmas.exception.InvalidMenuCountSeparatorException;
+import christmas.exception.InvalidMenusSeparatorException;
+import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class InputValidatorTest {
 
@@ -18,7 +24,7 @@ public class InputValidatorTest {
     }
 
     @ParameterizedTest
-    @DisplayName("숫자가 입력되었을떄 정상처리")
+    @DisplayName("날짜가 입력되었을떄 정상처리")
     @CsvSource({"'1'", "'2'", "'3'"})
     void validateNumberTest(String readLine) {
         Assertions.assertThatCode(() -> inputValidator.validateNumber(readLine))
@@ -41,4 +47,69 @@ public class InputValidatorTest {
                 .isInstanceOf(InputBlankException.class);
     }
 
+    @ParameterizedTest
+    @DisplayName("메뉴가 입력되었을떄 정상처리")
+    @CsvSource({"'피자-1,통닭-2,짜장면-3'"})
+    void validateMenuDataTest(String readLine) {
+        Assertions.assertThatCode(() -> inputValidator.validateMenuLine(readLine))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("[ERROR] 메뉴간,구분을 잘못하였을때")
+    @CsvSource({"'피자-1,통닭-2,,짜장면-3'", "',피자-1,통닭-2,짜장면-3'", "'피자-1,통닭-2,,짜장면-3,'"})
+    void validateMenuLineTest(String readLine) {
+        Assertions.assertThatThrownBy(() -> inputValidator.validateMenuLine(readLine))
+                .isInstanceOf(InvalidMenusSeparatorException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("[ERROR] 메뉴와개수간 -구분을 잘못 사용하였을때")
+    @MethodSource
+    void validateMenuCountSplitTest(List<String> menus) {
+        Assertions.assertThatThrownBy(() -> inputValidator.validateMenuCountSplit(menus))
+                .isInstanceOf(InvalidMenuCountSeparatorException.class);
+    }
+
+    private static Stream<Arguments> validateMenuCountSplitTest() {
+        return Stream.of(
+                Arguments.of(List.of("-피자-1", "통닭-2", "짜장면-3")),
+                Arguments.of(List.of("피자-1", "통닭--2", "짜장면-3")),
+                Arguments.of(List.of("피자-1", "통닭-2", "짜장면-3-"))
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("[ERROR] 분리된 메뉴이름이 잘못되었을때")
+    @MethodSource
+    void validateMenuTest(List<String[]> menus) {
+        Assertions.assertThatThrownBy(() -> inputValidator.validateMenu(menus))
+                .isInstanceOf(InputBlankException.class);
+    }
+
+    private static Stream<Arguments> validateMenuTest() {
+        return Stream.of(
+                Arguments.of(List.of(new String[]{"피자", "1"}, new String[]{"  ", "2"}, new String[]{"짜장면", "3"})),
+                Arguments.of(List.of(new String[]{"", "1"}, new String[]{"통닭", "2"}, new String[]{"짜장면", "3"}))
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("[ERROR] 분리된 메뉴개수가 잘못되었을때")
+    @MethodSource
+    void validateCountTest(List<String[]> menus, Exception exception) {
+        Assertions.assertThatThrownBy(() -> inputValidator.validateCount(menus))
+                .isInstanceOf(exception.getClass());
+    }
+
+    private static Stream<Arguments> validateCountTest() {
+        return Stream.of(
+                Arguments.of(List.of(new String[]{"피자", "하나"}, new String[]{"통닭", "2"}, new String[]{"짜장면", "3"}),
+                        IncludeCharException.exception),
+                Arguments.of(List.of(new String[]{"피자", "1"}, new String[]{"통닭", ""}, new String[]{"짜장면", "3"}),
+                        InputBlankException.exception),
+                Arguments.of(List.of(new String[]{"피자", "1"}, new String[]{"통닭", ""}, new String[]{"짜장면", "  "}),
+                        InputBlankException.exception)
+        );
+    }
 }
