b08612f10614d2e665f54931101764f699ab5999
jiohh <dkswlgh99@naver.com>
2023-11-13 16:28:44 +0900

    feat(service): Menu 생성 기능 구현

---
+++ src/main/java/christmas/service/MenuService.java
@@ -0,0 +1,27 @@
+package christmas.service;
+
+import christmas.domain.Menu;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.HashMap;
+
+public class MenuService {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MenuService() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public Menu fetchValidatedMenu() {
+        while (true) {
+            try {
+                HashMap<String, Integer> menus = inputView.readMenuData();
+                return new Menu(menus);
+            } catch (IllegalArgumentException exception) {
+                outputView.printErrorMessage(exception);
+            }
+        }
+    }
+}
