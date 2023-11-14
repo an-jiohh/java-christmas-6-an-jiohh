03bc07f1e943c789c5be20c90038a1b676df2cd7
jiohh <dkswlgh99@naver.com>
2023-11-14 22:36:21 +0900

    feat(constants): 기타 상수

---
+++ src/main/java/christmas/constants/Constants.java
@@ -0,0 +1,25 @@
+package christmas.constants;
+
+public class Constants {
+    public final static int DECEMBER_START_DATE = 1;
+    public final static int DECEMBER_END_DATE = 31;
+
+    public final static String MENUS_SEPARATOR = ",";
+    public final static String MENUS_SEPARATOR_DUPLICATE = ",,";
+    public final static String MENU_COUNT_SEPARATOR = "-";
+    public final static String MENU_COUNT_SEPARATOR_DUPLICATE = "--";
+
+    public final static int INITIAL_NUM = 0;
+    public final static int MENU_COUNT_SPLIT_LENGTH = 2;
+
+    public final static int MENU_POSITION = 0;
+    public final static int COUNT_POSITION = 1;
+
+    public final static int MAX_MENU_COUNT = 20;
+
+    public final static int MINIMUM_ORDER_PRICE = 10_000;
+    public final static int ZERO_DISCOUNT = 0;
+    public final static int ZERO_COUNT = 0;
+    public final static int DISCOUNT_CONVERT = -1;
+    public final static int GIFT_CRITERIA = 120_000;
+}
