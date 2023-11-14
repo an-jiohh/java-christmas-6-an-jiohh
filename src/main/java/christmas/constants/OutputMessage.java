27d51fade323a237114876b072f5da5dc9b9805c
jiohh <dkswlgh99@naver.com>
2023-11-14 17:42:40 +0900

    feat(constants): 출력 메시지 상수 설정

---
+++ src/main/java/christmas/constants/OutputMessage.java
@@ -0,0 +1,18 @@
+package christmas.constants;
+
+public class OutputMessage {
+    public static final String NUMBER_FORMAT = "###,###";
+    public static final String MENU_HEAD = "<주문 메뉴>";
+    public static final String ALL_PRICE_HEAD = "<할인 전 총주문 금액>";
+    public static final String GIFT_MENU_HEAD = "<증정 메뉴>";
+    public static final String ALL_DISCOUNT_HEAD = "<혜택 내역>";
+    public static final String ALL_BENEFITS_AMOUNT_HEAD = "<총혜택 금액>";
+    public static final String PAYMENT_AMOUNT_HEAD = "<할인 후 예상 결제 금액>";
+    public static final String NEGATIVE_MARK = "-";
+    public static final String DISCOUNT_AMOUNT_SEPARATOR = ": -";
+    public static final String COUNT_UNIT = "개";
+    public static final String PRICE_UNIT = "원";
+    public static final String ONE_CHAMPAGNE = "샴페인 1개";
+    public static final String NO_DISCOUNT = "없음";
+    public static final String BLANK = " ";
+}
