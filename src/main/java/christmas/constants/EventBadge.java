f480b41d2d12f598387cc37918c3fe3ff04bf4d5
jiohh <dkswlgh99@naver.com>
2023-11-14 20:55:56 +0900

    feat(constants): 이벤트 배지 상수 구현

---
+++ src/main/java/christmas/constants/EventBadge.java
@@ -0,0 +1,25 @@
+package christmas.constants;
+
+public enum EventBadge {
+
+    NONE("없음", 0),
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000);
+
+    private final String name;
+    private final int amount;
+
+    EventBadge(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
